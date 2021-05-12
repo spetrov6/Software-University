e721f7e55cc1d310ad94c0888e3b797710eacc09
Stefan Petrov <chefi.petrov@gmail.com>
2021-05-12 09:05:14 +0300

    New Solution

---
+++ ProgrammingBasics/WhileLoop-Exercise/05-Coins/Solution.py
@@ -0,0 +1,29 @@
+change = float(input())
+change_coins = int(change * 100)
+coins = 0
+while change_coins > 0:
+    if change_coins >= 200:
+        change_coins -= 200
+        coins += 1
+    elif change_coins >= 100:
+        change_coins -= 100
+        coins += 1
+    elif change_coins >= 50:
+        change_coins -= 50
+        coins += 1
+    elif change_coins >= 20:
+        change_coins -= 20
+        coins += 1
+    elif change_coins >= 10:
+        change_coins -= 10
+        coins += 1
+    elif change_coins >= 5:
+        change_coins -= 5
+        coins += 1
+    elif change_coins >= 2:
+        change_coins -= 2
+        coins += 1
+    elif change_coins >= 1:
+        change_coins -= 1
+        coins += 1
+print(coins)
