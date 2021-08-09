9b2d6ed2e13a6b40d4246d2058520debe10a7780
Stefan Petrov <chefi.petrov@gmail.com>
2021-08-09 22:08:40 +0300

    New Solution

--- ProgrammingFundamentals/List-Lab/01-StrangeZoo/Solution.py
+++
@@ -1,5 +0,0 @@
-tail = input()
-body = input()
-head = input()
-final_list = [head,body,tail]
-print(final_list)
--- ProgrammingFundamentals/List-Lab/02-Courses/Solution.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-name_list = []
-for i in range(n):
-    names = input()
-    name_list.append(names)
-print(name_list)
