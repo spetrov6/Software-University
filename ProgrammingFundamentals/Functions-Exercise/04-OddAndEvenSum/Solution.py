ba3f9891f585952070d5703c1b315729d2eb0153
Stefan Petrov <chefi.petrov@gmail.com>
2021-08-16 21:48:18 +0300

    New Solution

---
+++ ProgrammingFundamentals/Functions-Exercise/04-OddAndEvenSum/Solution.py
@@ -0,0 +1,10 @@
+def odd_even_sum(num):
+    even_sum = 0
+    odd_sum = 0
+    for i in num:
+        if int(i) % 2 == 0:
+            even_sum += int(i)
+        else:
+            odd_sum += int(i)
+    return f"Odd sum = {odd_sum}, Even sum = {even_sum}"
+print(odd_even_sum(input()))
