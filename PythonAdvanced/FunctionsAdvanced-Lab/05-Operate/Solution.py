bbc6e3f222599510317a26d47ccc1b1aba06a272
Stefan Petrov <chefi.petrov@gmail.com>
2021-09-25 11:53:03 +0300

    New Solution

---
+++ PythonAdvanced/FunctionsAdvanced-Lab/05-Operate/Solution.py
@@ -0,0 +1,10 @@
+from functools import reduce
+def operate(operator,*args):
+    if operator == "+":
+        return reduce(lambda x,y: x + y,args)
+    elif operator == "*":
+        return reduce(lambda x, y: x * y, args)
+    elif operator == "/":
+        return reduce(lambda x, y: x / y, args)
+    else:
+        return reduce(lambda x,y: x - y,args)
