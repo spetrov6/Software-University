cd338470addf3b60cfe8c0205764467130df41a1
Stefan Petrov <chefi.petrov@gmail.com>
2021-10-11 22:59:26 +0300

    New Solution

---
+++ PythonOOP/03-ClassesAndObjects-Exercise/08-Library/project/user.py
@@ -0,0 +1,38 @@
+class User:
+    def __init__(self, user_id: int, username: str):
+        self.user_id = user_id
+        self.username = username
+        self.books = []
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, library):
+        if author in library.books_available or author in library.rented_books:
+            if book_name in library.books_available.get(author):
+                if self.username not in library.rented_books:
+                    library.rented_books[self.username] = {book_name: days_to_return}
+                    library.books_available[author].remove(book_name)
+                    self.books.append(book_name)
+                    return f"{book_name} successfully rented for the next {days_to_return} days!"
+                library.rented_books[self.username][book_name] = days_to_return
+                library.books_available[author].remove(book_name)
+                self.books.append(book_name)
+                return f"{book_name} successfully rented for the next {days_to_return} days!"
+            else:
+                for book in library.rented_books.values():
+                    if book_name in book:
+                        return f'The book "{book_name}" is already rented and will be available in {book[book_name]} days!'
+
+    def return_book(self, author: str, book_name: str, library):
+        if book_name in self.books:
+            library.rented_books[self.username].pop(book_name)
+            library.books_available[author].append(book_name)
+            self.books.remove(book_name)
+        else:
+            return f"{self.username} doesn't have this book in his/her records!"
+
+    def info(self):
+        self.books.sort()
+        return ', '.join(self.books)
+
+    def __str__(self):
+        return f"{self.user_id}, {self.username}, {self.books}"
+
